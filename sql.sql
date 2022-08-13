239baed79bc9eb7e6bb40725323fbcf26b9f3499
abcdefg-png <74417586+abcdefg-png@users.noreply.github.com>
2022-08-13 12:09:53 +0800

    Add files via upload

---
+++ sql.sql
@@ -0,0 +1,33 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 8.0.22 : Database - hesuan_result_collection
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`hesuan_result_collection` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `hesuan_result_collection`;
+
+/*Table structure for table `xinan` */
+
+DROP TABLE IF EXISTS `xinan`;
+
+CREATE TABLE `xinan` (
+  `sno` varchar(255) DEFAULT NULL,
+  `sname` varchar(255) DEFAULT NULL,
+  `time_result` varchar(255) DEFAULT NULL,
+  `test_result` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
