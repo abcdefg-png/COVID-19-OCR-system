a1bbba654cfb758ac7b3852df662320f4f629c04
abcdefg-png <74417586+abcdefg-png@users.noreply.github.com>
2022-08-14 16:28:14 +0800

    Update hesuan.php

--- Web/hesuan.php
+++ Web/hesuan.php
@@ -1,7 +1,7 @@
 <?php
 header("Content-type: text/html; charset=utf-8");
-$userName='homework';
-$passWord='yt6871654';
+$userName='';
+$passWord='';
 $host='localhost';
 $dataBase='homework';
 $tbname='data1';
